b9faa26da51177df58a485ee903819fdaf90f84b
Vagelis1608 <Vagelis1608@users.noreply.github.com>
2025-08-20 14:25:12 +0300

    Fix keyboxer.py

--- keyboxer.py
+++ keyboxer.py
@@ -19,7 +19,7 @@ if not GITHUB_TOKEN:
 
 # Search query
 search_query = "<AndroidAttestation>"
-search_url = f"https://api.github.com/search/code?q={search_query}+NOT+repo%3Apperez39%2Fgoogle-keys"
+search_url = f"https://api.github.com/search/code?q={search_query}+NOT+repo:pperez39/google-keys"
 
 # Headers for the API request
 headers = {
